bcf2b894f0f3b8c64c1bfd6c81915c4c0d9bb8b8
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2024-01-24 14:43:49 -0500

    chat urls

--- chat/urls.py
+++ chat/urls.py
@@ -9,13 +9,13 @@ from chat.views import (
 
 urlpatterns = [
     # create room
-    path('create_room/', StartConversationView.as_view(), name='start_convo'),
+    path('create_room', StartConversationView.as_view(), name='start_convo'),
     # get room on initiator and receiver
     path('', ConversationView.as_view(), name='conversations'),
     # get conversation  all messages
-    path('conversation/<int:convo_id>/', GetConversationView.as_view(), name='get_conversation'),
+    path('conversation/<int:convo_id>', GetConversationView.as_view(), name='get_conversation'),
 
-    path('message_delete/<int:pk>/', DeleteChatSMSView.as_view()),
+    path('message_delete/<int:pk>', DeleteChatSMSView.as_view()),
 
 
 ]
