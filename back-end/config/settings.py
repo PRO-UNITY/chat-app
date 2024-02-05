6176aaa73f7e5dca0544945b1055b09bf9db3e64
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2024-02-05 00:17:20 -0500

    ss

--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -147,6 +147,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 # cors
 CORS_ALLOWED_ORIGINS = [
+    "https://api.prounity.uz",
     "http://localhost:3000",
     "http://localhost:5174",
     "http://localhost:5173",
