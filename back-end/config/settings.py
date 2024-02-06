cfaecca819314a7bbf9af91dd9276c8fd40e5715
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2024-02-05 19:41:47 -0500

    sd

--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-ASGI_APPLICATION = "config.asgi.application"
+# ASGI_APPLICATION = "config.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
